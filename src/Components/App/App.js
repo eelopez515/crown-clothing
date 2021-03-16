92dc32e946cb6db4d758512919b4e86aee818f65
eelopez515 <eelopez15@gmail.com>
2021-03-16 13:34:07 -0400

    Homepage
    Created files for the homepage

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//Components
+import Homepage from '../Homepage/Hompage'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
