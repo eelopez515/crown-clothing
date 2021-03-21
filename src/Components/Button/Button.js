af0920136817ffc7700b4d4a295d354ac7a89c42
eelopez515 <eelopez15@gmail.com>
2021-03-21 18:38:34 -0400

    added styling to collectionItem and Button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import './Button.sass'
+import React from 'react';
+import './Button.sass';
 
-function Button({ children, isGoogleSignIn, ...props }) {
-    return (
-        <button className={`${isGoogleSignIn ? 'google__sign__in' : ''} button`} {...props} >
-            {children}
-        </button>
-    )
+function Button({ children, isGoogleSignIn, inverted, ...props }) {
+  return (
+    <button
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google__sign__in' : ''
+      } button`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
--- src/Components/CollectionItem/CollectionItem.js
+++ src/Components/CollectionItem/CollectionItem.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import './CollectionItem.sass'
+import React from 'react';
+//Component
+import Button from '../Button/Button';
+
+import './CollectionItem.sass';
 
 function CollectionItem({ id, name, price, imageUrl }) {
-    return (
-        <div className='collection__item'>
-            <div
-            className="item__image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-            />
-            <div className="collection__footer">
-                <span className="item__name">{name}</span>
-                <span className="item__price">${price}</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="collection__item">
+      <div
+        className="item__image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection__footer">
+        <span className="item__name">{name}</span>
+        <span className="item__price">${price}</span>
+      </div>
+      <Button inverted>ADD TO CART</Button>
+    </div>
+  );
 }
 
-export default CollectionItem
+export default CollectionItem;
